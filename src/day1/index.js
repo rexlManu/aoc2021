f13a04431fa401e1255d17d8be07f295808479a0
Emmanuel Lampe <mail@emmanuel-lampe.de>
2021-12-01 14:47:37 +0100

    formated day1

--- src/day1/index.js
+++ src/day1/index.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
 const measurements = fs
-.readFileSync(__dirname+'/input', 'utf-8')
-.trim()
-.split(/\r?\n/)
-.map(Number);
+  .readFileSync(__dirname + '/input', 'utf-8')
+  .trim()
+  .split(/\r?\n/)
+  .map(Number);
 
-console.log(measurements.reduce((tot, cur, i, arr) => cur > arr[i - 1] ? tot + 1 : tot, 0));
+console.log(
+  measurements.reduce(
+    (tot, cur, i, arr) => (cur > arr[i - 1] ? tot + 1 : tot),
+    0,
+  ),
+);
