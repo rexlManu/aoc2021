1a313f2492c9277ca6fcaf7cebd53424d58e1534
Emmanuel Lampe <mail@emmanuel-lampe.de>
2021-12-01 16:21:32 +0100

    added part 2 of day 1

--- src/day1/index.js
+++ src/day1/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-const measurements = fs
+// Part 1
+let measurements = fs
   .readFileSync(__dirname + '/input', 'utf-8')
   .trim()
   .split(/\r?\n/)
@@ -12,3 +13,21 @@ console.log(
     0,
   ),
 );
+
+// Part 2
+const sumNumbers = (a, b) => a + b;
+
+let increasedTotal = 0;
+
+for (let index = 0; index < measurements.length - 2; index++) {
+  const currentWindow = measurements.slice(index, index + 3).reduce(sumNumbers);
+  const nextWindow = measurements
+    .slice(index + 1, index + 4)
+    .reduce(sumNumbers);
+
+  if (nextWindow > currentWindow) {
+    increasedTotal++;
+  }
+}
+
+console.log(increasedTotal);
